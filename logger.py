e2a1801ad61f0863f5584b17a9f3cb369aabea7d
Alex Ustas <alex.ustas@internet.ru>
2022-12-25 18:46:26 +0300

    final version

--- logger.py
+++ logger.py
@@ -6,9 +6,9 @@ def log_exec(expr: str, result: str):
     """Записывает в файл результат вычислений
     в виде |задача -> ответ|"""
     if os.path.exists('log.txt'):
-        file = open('log.txt', 'a')
+        file = open('log.txt', 'a', encoding='utf-8')
     else:
-        file = open('log.txt', 'w')
+        file = open('log.txt', 'w', encoding='utf-8')
     file.write(f'{expr} -> {result}\n')
     file.close()
 
@@ -16,7 +16,7 @@ def log_exec(expr: str, result: str):
 def get_history() -> str:
     """"Возвращает содержимое файла с результатами вычислений"""
     if os.path.exists('log.txt'):
-        with open('log.txt', 'r') as file:
+        with open('log.txt', 'r', encoding='utf-8') as file:
             return file.read()
     else:
         return 'Файл log.txt не существует'
--- model.py
+++ model.py
@@ -1,19 +1,34 @@
-# Модуль для выполнения опреаций
+# Модуль для выполнения операций
+import sympy
 
 
-def execute_expr(expr: str) -> str:         # (5+3)*10 -> 80
+def execute_expr(expr: str) -> str:  # (5+3)*10 -> 80
     """Принимает на вход строку-пример.
     Возвращает результат примера."""
-    pass
+    try:
+        result = str(sympy.simplify(expr))
+    except ValueError:
+        result = 'Неверный ввод'
+    return result
 
 
-def solve_eq(expr: str) -> str:                    # x**3 - 8 = 0 -> "2"
-                                                    # x**2 - 1 = 0 -> "1,-1"
+def solve_eq(expr: str) -> str:  # x**3 - 8 = 0 -> "2"
+    # x**2 - 1 = 0 -> "1,-1"
     """Принимает на вход уравнение в виде строки.
     Возвращает список корней уравнения в строку с разделителем"""
-    pass
+    x = sympy.Symbol('x')
+    try:
+        temp = list(map(str, sympy.solve(expr, x)))
+        result = ','.join(temp)
+    except ValueError:
+        result = 'Неверный ввод'
+    return result
 
 
-def simpify_pol(expr: str) -> str:           # x**2 + 3*x**2 + 4 -> 4*x**2 + 4
+def simplify_pol(expr: str) -> str:  # x**2 + 3*x**2 + 4 -> 4*x**2 + 4
     """"Упрощает введенный многочлен"""
-    pass
+    try:
+        result = str(sympy.simplify(expr))
+    except ValueError:
+        result = 'Неверный ввод'
+    return result
